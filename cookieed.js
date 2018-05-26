ac0b92107c9580357cbfa1bfd0725cd0ec3d3cca
Clive Machado <clive.machado@raweng.com>
2018-05-26 11:02:45 +0530

    cookieed: - first commit.

---
+++ cookieed.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var cookieed = (function(){
+
+  var _ = {
+    clive : function() {
+      console.log("clive");
+    } 
+  }
+
+  return {
+    init : function (){
+      
+    },
+    setCookieKey: function(){
+      _.clive();
+    }
+  };
+})();
